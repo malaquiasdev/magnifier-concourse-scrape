532afc5d50d94ff94e89aa876effa5d6ddfeb135
Mateus Malaquias <mateusmalaquiasdev@outlook.com.br>
2022-01-09 11:41:33 -0300

    feat: new funciton to get the anwsers

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -28,6 +28,26 @@ resource "aws_lambda_function" "lambda_magnifier_scrape_qconcursos_questions_pag
   }
 }
 
+resource "aws_lambda_function" "qconcursos_answers_page" {
+  function_name    = "${var.lambda_qconcursos_prefix_name}-answerspage"
+  handler          = "./qconcrusos/anwsers/index.handler"
+  description      = "Scrape the qconcursos answers page by filter and question id"
+  runtime          = "nodejs14.x"
+  timeout          = 900
+  memory_size      = 1024
+  role             = aws_iam_role.lambda_magnifier_scrape_qconcursos_questions_page_role.arn
+  s3_bucket        = aws_s3_bucket.bucket_root.id
+  s3_key           = aws_s3_bucket_object.lambda_functions_bucket_object.key
+  source_code_hash = data.archive_file.functions_artefact.output_base64sha256
+  layers           = [aws_lambda_layer_version.layer_components.arn]
+  environment {
+    variables = {
+      TABLE_NAME          = "${var.project_name}-questions"
+      QCONCURSOS_BASE_URL = "https://www.qconcursos.com"
+    }
+  }
+}
+
 resource "aws_lambda_permission" "lambda_magnifier_scrape_qconcursos_questions_page_api" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_magnifier_scrape_qconcursos_questions_page.arn
