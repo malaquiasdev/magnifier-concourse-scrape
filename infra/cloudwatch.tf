4bafea8ae2574bbbf540b31994c68b2719b2a3b8
Mateus Malaquias <mateusmalaquiasdev@outlook.com.br>
2022-01-10 22:55:16 -0300

    delete: infra as code to start a new one

--- infra/cloudwatch.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "this" {
-  name              = "/aws/lambda/${aws_lambda_function.lambda_magnifier_scrape_qconcursos_questions_page.function_name}"
-  retention_in_days = 3
-}
--- infra/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  app_functions_path = "../${path.module}/app/src/functions"
-  lambdas_path       = "../${path.module}/build/dist"
-  layers_path        = "../${path.module}/app/src/layers"
-}
