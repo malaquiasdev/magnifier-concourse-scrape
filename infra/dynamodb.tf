dd11e7920b9e556b3d5fae4357323e22cf7349ae
Mateus Malaquias <mateusmalaquiasdev@outlook.com.br>
2022-01-10 23:27:39 -0300

    refactor: dynamodb infra, now we have two tables

---
+++ infra/dynamodb.tf
@@ -0,0 +1,19 @@
+resource "aws_dynamodb_table" "qconcursos_audity" {
+  name         = "${var.project_name}-audity"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "requestId"
+  attribute {
+    name = "requestId"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "qconcursos_questions" {
+  name         = "${var.project_name}-questions"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "questionId"
+  attribute {
+    name = "questionId"
+    type = "S"
+  }
+}
